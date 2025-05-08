a22fadcc48d8e8db79197ff15a781aa2c53fd40a
Keloran <keloran@develbox.info>
2025-05-08 08:44:44 +0100

    feat(pnpm-lock.yaml): Add new Radix UI dependencies
    This commit adds new Radix UI dependencies to the `pnpm-lock.yaml` file, including `@radix-ui/react-arrow`, `@radix-ui/react-dropdown-menu`, and related peer dependencies. These changes are necessary to support the implementation of new UI components in the application.

---
+++ app/components/header/theme/index.tsx
@@ -0,0 +1,28 @@
+import { useFlags } from "@flags-gg/react-library";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "~/components/ui/dropdown-menu";
+import {MoonIcon, SunIcon, SunMoonIcon, MonitorCogIcon} from "lucide-react";
+import {themeAtom} from "~/lib/state";
+import {useAtom} from "jotai";
+
+export const Theme = () => {
+  const {is} = useFlags();
+  const [_, setTheme] = useAtom(themeAtom)
+
+  if (!is("darkmode").enabled()) return null
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <SunMoonIcon className={"cursor-pointer"} />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align={"end"}>
+        <DropdownMenuItem className={"cursor-pointer"} onClick={() => setTheme("light")}>
+          <SunIcon />&nbsp; Light
+        </DropdownMenuItem>
+        <DropdownMenuItem className={"cursor-pointer"} onClick={() => setTheme("dark")}>
+          <MoonIcon />&nbsp; Dark
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
